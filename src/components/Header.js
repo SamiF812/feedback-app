c6e0327c72ec7cfdab98e0f432766579fb7a286b
Sami Fares <djsamif@gmail.com>
2022-05-16 15:33:49 -0400

    got the feedback displaying on the page by passing in props

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,15 @@ import PropTypes from 'prop-types'
 
 import React from 'react'
 
-function Header({ text }) {
-  return (
-    <header>
+function Header({ text, bgColor, textColor }) {
+  
+  const headerStyles = {
+      backgroundColor: bgColor,
+      color: textColor,
+  }
+  
+    return (
+    <header style={headerStyles}>
         <div className='container'>
             <h2>{text}</h2>
         </div>
@@ -14,11 +20,16 @@ function Header({ text }) {
 }
 Header.defaultProps = {
     text: 'Feedback UI',
+    bgColor: 'rgba(0,0,0,0.4)',
+    textColor: '#ff6a95',
 }
 
 
 Header.propTypes = {
-    text: PropTypes.string
+    text: PropTypes.string,
+    bgColor: PropTypes.string,
+    textColor: PropTypes.string
+
 }
 
 export default Header
