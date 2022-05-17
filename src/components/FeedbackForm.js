5db115f91af56f4212b65959e720bc2fbbef4c84
Sami Fares <djsamif@gmail.com>
2022-05-17 15:21:47 -0400

    completed the feedback form and submit

--- src/components/FeedbackForm.js
+++ src/components/FeedbackForm.js
@@ -1,27 +1,61 @@
 import React from "react";
 import Card from "./shared/Card";
-import {useState} from 'react';
+import { useState } from "react";
+import Button from "./shared/Button";
+import RatingSelect from "./RatingSelect";
 
-function FeedbackForm() {
-    const [text, setText] = useState('')
+function FeedbackForm({handleAdd}) {
+    const [text, setText] = useState("");
+    const [rating, setRating] = useState(10);
+    const [btnDisabled, setBtnDisabled] = useState(true);
+    const [message, setMessage] = useState("");
 
     const handleTextChange = (e) => {
-        setText(e.target.value)
-    }
+        if (text === "") {
+            setBtnDisabled(true);
+            setMessage(null);
+        } else if (text !== "" && text.trim().length <= 10) {
+            setMessage("Text must be atleast 10 characters");
+            setBtnDisabled(true);
+        } else {
+            setMessage(null);
+            setBtnDisabled(false);
+        }
+
+        setText(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (text.trim().length > 10) {
+            const newFeedback = {
+                text,
+                rating,
+            }
+
+            handleAdd(newFeedback)
+
+            setText('')
+
+        }
+    };
     return (
         <Card>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h2>How would you rate your service with us?</h2>
-            
-            <div className="input-group">
-                <input onChange={handleTextChange} 
-                type='text' 
-                placeholder="Write a review"
-                value={text}
-                />
-                <button type='submit'>Send</button>
-            </div>
-            
+                <RatingSelect select={setRating} selected={rating} />
+                <div className="input-group">
+                    <input
+                        onChange={handleTextChange}
+                        type="text"
+                        placeholder="Write a review"
+                        value={text}
+                    />
+                    <Button type="submit" isDisabled={btnDisabled}>
+                        Send
+                    </Button>
+                </div>
+                {message && <div className="message">{message}</div>}
             </form>
         </Card>
     );
