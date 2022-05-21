5de08d4b0fa6008f3f099a84426d266d727a8961
Sami Fares <djsamif@gmail.com>
2022-05-21 10:20:53 -0400

    cleaned up ratingselect imports

--- src/components/RatingSelect.js
+++ src/components/RatingSelect.js
@@ -1,9 +1,7 @@
 import React from "react";
-import { useState, useContext, useEffect } from "react"
-import FeedbackContext from "../components/context/FeedbackContext";
 
 function RatingSelect({ select, selected }) {
-  // const [selected, setSelected] = useState(10)
+    // const [selected, setSelected] = useState(10)
     // NOTE: We don't need local state here as it's a duplicate of parent state
     // also no real need for useEffect or context
     // useEffect(() => {
@@ -16,29 +14,29 @@ function RatingSelect({ select, selected }) {
     //   console.log(feedbackEdit)
     //   setSelected(feedbackEdit.item.rating)
     // }, [feedbackEdit])
-  
+
     const handleChange = (e) => {
-      select(+e.currentTarget.value)
-    }
-  
+        select(+e.currentTarget.value);
+    };
+
     // NOTE: simplified with iteration
     return (
-      <ul className='rating'>
-        {Array.from({ length: 10 }, (_, i) => (
-          <li key={`rating-${i + 1}`}>
-            <input
-              type='radio'
-              id={`num${i + 1}`}
-              name='rating'
-              value={i + 1}
-              onChange={handleChange}
-              checked={selected === i + 1}
-            />
-            <label htmlFor={`num${i + 1}`}>{i + 1}</label>
-          </li>
-        ))}
-      </ul>
-    )
-  }
-  
-  export default RatingSelect
+        <ul className="rating">
+            {Array.from({ length: 10 }, (_, i) => (
+                <li key={`rating-${i + 1}`}>
+                    <input
+                        type="radio"
+                        id={`num${i + 1}`}
+                        name="rating"
+                        value={i + 1}
+                        onChange={handleChange}
+                        checked={selected === i + 1}
+                    />
+                    <label htmlFor={`num${i + 1}`}>{i + 1}</label>
+                </li>
+            ))}
+        </ul>
+    );
+}
+
+export default RatingSelect;
